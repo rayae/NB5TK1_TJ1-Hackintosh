6bb62bfcd6c995e2c5a5ad2891487afb4748e565
bavelee <bavelee@foxmail.com>
2020-03-27 12:17:06 +0800

    20200327

--- myefi.sh
+++ myefi.sh
@@ -17,37 +17,46 @@ fi
 xhange_config(){
 change_config "$2" "$1"
 }
+setCode(){
+	p=`grep -n "<key>$1</key>" $config|head -n 1|cut -d ':' -f1`
+	let p=p+1
+	sed -i "" "${p}s#<.*>#$2#" $config
+}
 reset_uuid(){
-xhange_config "C02727130CDHWVPJC" "MLB0000"
-xhange_config "U08lML6w" "ROM0000"
-xhange_config "C02V10AFHV29" "SSN0000"
-xhange_config "F0960000-0000-1000-8000-80FA5B6077D1" "UUID0000"
+setCode MLB "<string>C02727130CDHWVPJC</string>"
+setCode ROM "<data>U08lML6w</data>"
+setCode SystemSerialNumber "<string>C02V10AFHV29</string>"
+setCode SystemUUID "<string>F0960000-0000-1000-8000-80FA5B6077D1</string>"
 echo "UUID 已恢复"
 }
 clear_uuid(){
-change_config "C02727130CDHWVPJC" "MLB0000"
-change_config "U08lML6w" "ROM0000"
-change_config "C02V10AFHV29" "SSN0000"
-change_config "F0960000-0000-1000-8000-80FA5B6077D1" "UUID0000"
+setCode MLB "<string>000000000000000000000</string>"
+setCode ROM "<data>00000000</data>"
+setCode SystemSerialNumber "<string>000000000000</string>"
+setCode SystemUUID "<string>00000000-0000-0000-0000-000000000000</string>"
 echo "UUID 已清除"
+}
+open_uiscale(){
+	echo "修改 UISCale 为\t: 02"
+	setCode UIScale "<data>Ag==</data>"
+}
+close_uiscale(){
+	echo "修改 UISCale 为\t: 01"
+	setCode UIScale "<data>AQ==</data>"
 }
 resmy(){
 	echo "清除我的配置文件"
 	cp -f $config $config4k
 	echo "删除 2160p 启动参数\t: -igfxmlr -cdfon"
 	/usr/bin/sed -i "" "s/-alcid=29 -igfxmlr -cdfon/-alcid=29/g" $config
-	echo "修改为 1080p 分辨率\t: 3840×2160 -> 1920x1080"
-	/usr/bin/sed -i "" "s/3840×2160/1920x1080/g" $config
-	echo "修改 UISCale 为\t: 01"
-	/usr/bin/sed -i "" "s/Ag==/AQ==/g" $config
-	echo "已生成 1080p 配置文件\t: $config"
+	close_uiscale
 	clear_uuid
+	echo "已生成 1080p 配置文件\t: $config"
 }
 usemy(){
 	echo "使用我的配置文件"
 	cp -f $config4k $config
-	echo "修改 UISCale 为\t: 02"
-	/usr/bin/sed -i "" "s/AQ==/Ag==/g" $config
+	open_uiscale
 	reset_uuid
 }
 autoconfig(){
@@ -55,8 +64,6 @@ autoconfig(){
 	cp -f $config $config4k
 	echo "增加 2160p 启动参数\t: -igfxmlr -cdfon"
 	/usr/bin/sed -i "" "s/-alcid=29/-alcid=29 -igfxmlr -cdfon/g" $config4k
-	echo "修改为 2160p 分辨率\t: 1920x1080 -> 3840×2160"
-	/usr/bin/sed -i "" "s/1920x1080/3840×2160/g" $config4k
 	echo "已生成 2160p 配置文件\t: $config4k"
 }
 make(){
