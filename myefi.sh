fb05b8e3f751eb2a79a0bb23185e14bb7e928a03
bavelee <bavelee@foxmail.com>
2021-06-08 18:50:21 +0800

    20210608:OC0.70.

--- myefi.sh
+++ myefi.sh
@@ -3,18 +3,38 @@ cwd=$(dirname $0)
 config=$cwd/OC/config.plist
 config4k=$cwd/OC/config_4k.plist
 backup_path=~/UUID-Backup
-getCode(){
+getUUID(){
 	if test ! -f $backup_path/$1;then
 		echo null
 		return
 	fi
 	echo `cat $backup_path/$1`
 }
-
-mlb=`getCode MLB`
-rom=`getCode ROM`
-ssn=`getCode SystemSerialNumber`
-uuid=`getCode SystemUUID`
+getValue(){
+	if test ! -f $config;then
+		echo "config 文件不存在"
+		return
+	fi
+	p=`grep -n "<key>$1</key>" $config|head -n 1|cut -d ':' -f1`
+	let p=p+1
+	value=`sed "${p}q;d" $config|sed "s#$(echo '\t')\+>##"`
+	value=`echo $value|sed  "s#<##"|sed "s#/>##"`
+	if [[ $value == true || $value == false ]]; then
+		echo $value
+		return
+	fi
+	value=`echo $value|sed "s#<.*>##"|sed "s#.*>##"`
+	echo $value
+}
+setValue(){
+	p=`grep -n "<key>$1</key>" $config|head -n 1|cut -d ':' -f1`
+	let p=p+1
+	sed -i "" "${p}s#<.*>#$2#" $config
+}
+mlb=`getUUID MLB`
+rom=`getUUID ROM`
+ssn=`getUUID SystemSerialNumber`
+uuid=`getUUID SystemUUID`
 
 usage(){
 echo "
@@ -28,45 +48,47 @@ if test -z $1;then
 fi
 /usr/bin/sed -i "" "s/$1/$2/g" $config
 }
-xhange_config(){
-change_config "$2" "$1"
-}
-setCode(){
-	p=`grep -n "<key>$1</key>" $config|head -n 1|cut -d ':' -f1`
-	let p=p+1
-	sed -i "" "${p}s#<.*>#$2#" $config
-}
-
 reset_uuid(){
-setCode MLB "<string>${mlb}</string>"
-setCode ROM "<data>${rom}</data>"
-setCode SystemSerialNumber "<string>${ssn}</string>"
-setCode SystemUUID "<string>${uuid}</string>"
+setValue MLB "<string>${mlb}</string>"
+setValue ROM "<data>${rom}</data>"
+setValue SystemSerialNumber "<string>${ssn}</string>"
+setValue SystemUUID "<string>${uuid}</string>"
 echo "UUID 已恢复"
 }
 clear_uuid(){
 remove_macos_files
-setCode MLB "<string>000000000000000000000</string>"
-setCode ROM "<data>00000000</data>"
-setCode SystemSerialNumber "<string>000000000000</string>"
-setCode SystemUUID "<string>00000000-0000-0000-0000-000000000000</string>"
+setValue MLB "<string>000000000000000000000</string>"
+setValue ROM "<data>00000000</data>"
+setValue SystemSerialNumber "<string>000000000000</string>"
+setValue SystemUUID "<string>00000000-0000-0000-0000-000000000000</string>"
 echo "UUID 已清除"
 }
 enable_4k(){
 	echo "为 4K 屏幕设置参数"
-	setCode dpcd-max-link-rate "<data>FAAAAA==</data>"
-	setCode UIScale "<data>Ag==</data>"
+	setValue dpcd-max-link-rate "<data>FAAAAA==</data>"
+	setValue UIScale "<data>Ag==</data>"
 }
 disable_4k(){
 	echo "为 1080P 屏幕设置参数"
-	setCode dpcd-max-link-rate "<data>CgAAAA==</data>"
-	setCode UIScale "<data>AQ==</data>"
+	setValue dpcd-max-link-rate "<data>CgAAAA==</data>"
+	setValue UIScale "<data>AQ==</data>"
+}
+enable_picker(){
+	echo "启用 ShowPicker"
+	setValue ShowPicker "<true/>"
+	setValue Timeout "<integer>5</integer>"
+}
+disable_picker(){
+	echo "禁用 ShowPicker"
+	setValue ShowPicker "<false/>"
+	setValue Timeout "<integer>0</integer>"
 }
 usemy(){
 	echo "恢复我的配置文件"
 	cp -f $config4k $config
 	rm -f $config4k
 	enable_4k
+	disable_picker
 	reset_uuid
 }
 autoconfig(){
@@ -74,14 +96,16 @@ autoconfig(){
 	cp -f $config $config4k
 	echo "已生成 4K屏 配置文件\t: $config4k"
 	disable_4k
+	enable_picker
 	echo "已生成 1080P屏 配置文件\t: $config"
 }
 make(){
 if test -z "$1";then
-	echo "less than 1 arguments"
+	echo "指定输出文件名"
 	exit 1
 fi
 autoconfig
+remove_macos_files
 cd ..
 zip -qr "$1" EFI/BOOT EFI/OC EFI/附加工具 EFI/README.md
 cd - >/dev/null
@@ -92,6 +116,7 @@ if test -z "$1";then
 	echo "less than 1 arguments"
 	exit 1
 fi
+remove_macos_files
 autoconfig
 git add .
 git commit -m "$1"
@@ -116,8 +141,6 @@ elif [[ "$1"x == "push"x ]];then
 elif [[ "$1"x == "rmf"x ]];then
 		remove_macos_files
 fi
-
-
 
 
 
